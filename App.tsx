f8402381770b52edbb6f6ce6419aab8b882a6e8b
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-05 01:40:11 -0300

    Installs styled-component library and configures initial theme and home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans"
+import { StatusBar } from "react-native"
+import { ThemeProvider } from "styled-components/native"
+
+import theme from "@/theme"
+import { Loading } from "@/components/Loading"
+import { Home } from "@/screens/Home"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Home /> : <Loading />}
+    </ThemeProvider>
+  )
+}
